786241ca21e412da66b6181910f36beebf7825d1
Osuagwu Emeka <emekaosuagwu@hotmail.com>
2016-07-20 20:46:11 +0100

    update app

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,22 +11,22 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    return [
-        'role' 				=> 1,
-        'email' 			=> "admin@oyster.com",
-        'password' 			=> "password",
-        'profile_status' 	=> 0,
-        'remember_token' 	=> str_random(10),
-    ];
-});
-
 // $factory->define(App\User::class, function (Faker\Generator $faker) {
 //     return [
-//         'role' 				=> rand(1, 3),
-//         'email' 			=> $faker->email,
+//         'role' 				=> 1,
+//         'email' 			=> "admin@oyster.com",
 //         'password' 			=> "password",
-//         'profile_status' 	=> rand(0, 1),
+//         'profile_status' 	=> 0,
 //         'remember_token' 	=> str_random(10),
 //     ];
 // });
+
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+    return [
+        'role' 				=> rand(1, 3),
+        'email' 			=> $faker->email,
+        'password' 			=> "password",
+        'profile_status' 	=> rand(0, 1),
+        'remember_token' 	=> str_random(10),
+    ];
+});

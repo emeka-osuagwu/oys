6d28e512f000ec6085bee9d1f476203fc98810e4
Osuagwu Emeka <emekaosuagwu@hotmail.com>
2016-07-21 12:06:24 +0100

    update app name

--- resources/views/pages/includes/sections/header.blade.php
+++ resources/views/pages/includes/sections/header.blade.php
@@ -3,7 +3,7 @@
     <!-- LOGO -->
     <div class="topbar-left">
         <div class="text-center">
-            <a href="index.html" class="logo"><i class="md md-terrain"></i> <span>Moltran </span></a>
+            <a href="index.html" class="logo"><i class="md md-terrain"></i> <span>Oyster </span></a>
         </div>
     </div>
     <!-- Button mobile view to collapse sidebar menu -->
